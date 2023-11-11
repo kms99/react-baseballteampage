a621d5ae42f29518370c8e76231d077e44bad350
Kim MyungSub <audtjqxx@naver.com>
2023-11-12 04:04:14 +0900

    header title 클릭시 Home 페이지로 navigate 기능구현

--- src/components/Header/HeaderTitle.jsx
+++ src/components/Header/HeaderTitle.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { initTeams } from "../../commonData";
+import { useNavigate } from "react-router-dom";
 
 const StTitle = styled.h1`
   font-size: 4rem;
   font-family: "BlackHanSans", sans-serif;
   color: ${(props) => props.theme.subColor[initTeams[props.selected].team]};
   transition: all 0.5s;
+  cursor: pointer;
 `;
 const HeaderTitle = ({ selectTeam }) => {
-  return <StTitle selected={selectTeam}>프로야구 TALK</StTitle>;
+  const navigate = useNavigate();
+  const goToHomeHandler = () => {
+    navigate("/");
+  };
+  return (
+    <StTitle selected={selectTeam} onClick={goToHomeHandler}>
+      프로야구 TALK
+    </StTitle>
+  );
 };
 
 export default HeaderTitle;
