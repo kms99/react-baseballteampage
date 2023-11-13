2e1a8d97d0eeb398bae49be929d38603b264d421
Kim MyungSub <audtjqxx@naver.com>
2023-11-13 17:30:11 +0900

    안쓰는 모듈 제거

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,13 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Detail from "../pages/Detail";
 import Header from "../components/Header/Header";
-import { useEffect, useState } from "react";
 import { styled, ThemeProvider } from "styled-components";
 import GlobalStyle from "../GlobalStyle";
 import GlobalFont from "../style/fonts";
 import theme from "../theme";
 import background from "../image/background.jpg";
-import { useSelector } from "react-redux";
 
 const Router = () => {
   return (
